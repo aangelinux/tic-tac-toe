614a1645ccdffbb234efdfca4013d4777312d7e6
Angelina Larsson <al227bn@student.lnu.se>
2025-11-18 10:39:54 +0100

    REFACTOR: Board draws a mark on clicked tile

--- src/board.js
+++ src/board.js
@@ -12,6 +12,13 @@ export class Board {
 	}
 
 	drawMark(clickedTile) {
+		const nought = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+		nought.setAttribute("r", 45)
+		nought.setAttribute("fill", "none")
+		nought.setAttribute("stroke", "black")
+		nought.setAttribute("stroke-width", 3)
+
+		clickedTile.appendChild(nought)
 		clickedTile.marked = true
 	}
 }
