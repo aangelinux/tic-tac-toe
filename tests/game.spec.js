16db75ab4543c03a13b2593c251bbdca6d4f3b44
Angelina Larsson <al227bn@student.lnu.se>
2025-11-19 16:35:45 +0100

    RED: Game should tell Board to mark AI's tile

--- tests/game.spec.js
+++ tests/game.spec.js
@@ -11,8 +11,7 @@ import { RandomStub } from "./__mocks__/random.js"
 describe("Game", () => {
 	it("should give turn to AI when human has played", () => {
 		//Arrange
-		const random = new RandomStub()
-		const game = new Game(new Board(), new AI(random))
+		const game = new Game(new Board(), new AI(new RandomStub()))
 		const giveTurnToAI = jest.spyOn(game, "giveTurnToAI") // TODO fix coupling to implm
 
 		//Act
@@ -25,5 +24,18 @@ describe("Game", () => {
 
 		//Assert
 		expect(giveTurnToAI).toHaveBeenCalledTimes(1)
+	})
+
+	it("should tell Board to mark AI's chosen tile", () => {
+		//Arrange
+		const board = new Board()
+		const game = new Game(board, new AI(new RandomStub()))
+
+		//Act
+		game.start()
+		game.giveTurnToAI()
+
+		//Assert
+		expect(board.tiles[8].querySelector("svg")).toBeInstanceOf(SVGAElement)
 	})
 })
