860f664382ad65ccd629571aee8be5595e0a4703
Angelina Larsson <al227bn@student.lnu.se>
2025-11-20 22:35:29 +0100

    RED: Board should create Tile objects

--- tests/board.spec.js
+++ tests/board.spec.js
@@ -4,8 +4,19 @@
 
 import { describe, it, expect, jest } from "@jest/globals"
 import { Board } from "../src/view/board/board.js"
+import { Tile } from "../src/view/tile/tile.js"
 
 describe("Board", () => {
-	it("should create Tile instances", () => {
+	it("should create Tile objects", () => {
+		//Arrange
+		const board = new Board()
+		const size = 9
+
+		//Act
+		board.draw(size)
+
+		//Assert
+		expect(board.tiles).toHaveLength(size)
+		expect(board.tiles[0]).toBeInstanceOf(Tile)
 	})
 })
