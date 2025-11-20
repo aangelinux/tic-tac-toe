3e15fbf488820623e630532c5387dee3f0bc498b
Angelina Larsson <al227bn@student.lnu.se>
2025-11-20 16:28:13 +0100

    RED: Tile should draw mark when player clicks on empty tile

--- tests/tile.spec.js
+++ tests/tile.spec.js
@@ -2,15 +2,16 @@
  * Unit tests for the board tiles.
  */
 
+import "@testing-library/jest-dom"
 import { describe, it, expect, jest } from "@jest/globals"
 import { Tile } from "../src/view/tile/tile"
 
 describe("Tile", () => {
-	it("should check if it is empty or marked when player clicks on it", () => {
+	it("should check if it's empty or marked when player clicks on it", () => {
 		//Arrange
 		const tile = new Tile()
 		document.body.appendChild(tile)
-		
+
 		const isMarked = jest.spyOn(tile, "isMarked")
 
 		//Act
@@ -18,5 +19,19 @@ describe("Tile", () => {
 
 		//Assert
 		expect(isMarked).toHaveBeenCalledTimes(1)
+	})
+
+	it("should draw a mark when player clicks on an empty tile", () => {
+		//Arrange
+		const tile = new Tile()
+		document.body.appendChild(tile)
+
+		jest.spyOn(tile, "isMarked").mockReturnValueOnce(false)
+
+		//Act
+		tile.click()
+
+		//Assert
+		expect(tile).toContainElement(SVGSVGElement)
 	})
 })
