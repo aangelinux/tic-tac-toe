35d93423875069e3a1ddd435b738d797c2aa5b02
Angelina Larsson <al227bn@student.lnu.se>
2025-11-26 17:56:56 +0100

    Board should fire event when there are three in a row

--- tests/board.spec.js
+++ tests/board.spec.js
@@ -2,7 +2,7 @@
  * Unit tests for the game board.
  */
 
-import { describe, it, expect } from "@jest/globals"
+import { describe, it, expect, jest } from "@jest/globals"
 import { Board } from "../src/view/board/board.js"
 import { Tile } from "../src/view/tile/tile.js"
 
@@ -18,5 +18,20 @@ describe("Board", () => {
 		//Assert
 		expect(board.tiles).toHaveLength(size)
 		expect(board.tiles[0]).toBeInstanceOf(Tile)
+	})
+
+	it("should fire event if there are three marks in a row", () => {
+		//Arrange
+		const board = new Board()
+		const size = 9
+		const mock = jest.fn()
+		document.addEventListener("three-in-row", mock)
+
+		//Act
+		board.draw(size)
+		jest.spyOn(board, "hasThreeInARow").mockReturnValueOnce(true)
+
+		//Assert
+		expect(mock).toHaveBeenCalledTimes(1)
 	})
 })
