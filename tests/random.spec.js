6222160c1bd0aa5fce20a088964c2c919a7f43d8
Angelina Larsson <al227bn@student.lnu.se>
2025-12-02 13:25:35 +0100

    RED: Random should generate a value equal to or greater than 0

--- tests/random.spec.js
+++ tests/random.spec.js
@@ -16,4 +16,15 @@ describe("Random", () => {
 		//Assert
 		expect(random.value).toBeLessThanOrEqual(8)
 	})
+
+	it("should generate a number equal to or greater than 0", () => {
+		//Arrange
+		const random = new Random()
+
+		//Act
+		const value = random.value
+
+		//Assert
+		expect(value).toBeGreaterThanOrEqual(0)	
+	})
 })
