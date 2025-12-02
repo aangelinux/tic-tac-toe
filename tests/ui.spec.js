6a449b7db822ae0bc602fca077aa58e0aa7b7b65
Angelina Larsson <al227bn@student.lnu.se>
2025-12-02 15:18:07 +0100

    RED: UI should update its content when a new turn starts

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -3,10 +3,23 @@
  */
 
 import '@testing-library/jest-dom'
-import { describe, it, expect } from "@jest/globals"
+import { describe, it, expect, jest } from "@jest/globals"
 import { UI } from "../src/view/ui/ui"
 
 describe("UI", () => {
+	it("should update its content when a new turn starts", () => {
+		//Arrange
+		const ui = new UI()
+		const newTurn = new CustomEvent("new-turn")
+		const update = jest.spyOn(ui, "update")
+
+		//Act
+		ui.dispatchEvent(newTurn)
+
+		//Assert
+		expect(update).toHaveBeenCalledTimes(1)
+	})
+
 	it("should say 'Player: Human' when it's the player's turn", () => {
 		//Arrange
 		const ui = new UI()
